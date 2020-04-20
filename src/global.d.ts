d57dbcd7f341438ca312137fe473d70aec6dc124
Ian Sanders <iansan5653@gmail.com>
2020-04-19 20:07:57 -0400

    Initial commit

---
+++ src/global.d.ts
@@ -0,0 +1,3 @@
+/** Add functions to this object to expose them to Google Apps triggers. */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare const global: Record<string, (event: any) => void>;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
+/* eslint-disable no-undef, @typescript-eslint/no-var-requires */
+const GasPlugin = require("gas-webpack-plugin");
+
 module.exports = {
-  mode: 'production',
-  entry: './build',
+  mode: "production",
+  context: __dirname,
+  entry: "./build/index.js",
   output: {
     path: __dirname,
-    filename: 'Code.js',
-    libraryTarget: "var"
-  }
+    filename: "Code.js",
+  },
+  plugins: [new GasPlugin()],
+  devtool: false,
 };
