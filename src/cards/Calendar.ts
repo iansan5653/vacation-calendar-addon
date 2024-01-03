b4bc4c53eb49f62d7190f2b1ed0b3f7aed769ba0
Ian Sanders <iansan5653@github.com>
2024-01-03 18:53:50 +0000

    Add linked google calendar section

--- src/cards/Calendar.ts
+++ src/cards/Calendar.ts
@@ -21,21 +21,45 @@ function CalendarSettingsSection(key: TeamCalendarKey, calendar: TeamCalendar) {
   return CardService.newCardSection()
     .setHeader("Settings")
     .addWidget(
-      CardService.newKeyValue()
+      CardService.newDecoratedText()
         .setTopLabel("Team members")
-        .setContent(calendar.teamMembers.map((e) => ` - ${e}`).join("\n")),
+        .setText(calendar.teamMembers.map((e) => ` - ${e}`).join("\n")),
     )
     .addWidget(
-      CardService.newKeyValue()
+      CardService.newDecoratedText()
         .setTopLabel("Minimum event duration")
-        .setContent(`${calendar.minEventDuration} hours`),
+        .setText(`${calendar.minEventDuration} hours`),
     )
     .addWidget(CalendarActions(key));
+}
+
+function LinkedCalendarSection(calendar: TeamCalendar) {
+  const section = CardService.newCardSection().setHeader("Linked Google calendar");
+
+  let googleCalendar;
+  try {
+    googleCalendar = CalendarApp.getCalendarById(calendar.googleCalendarId);
+  } catch {
+    return section.addWidget(
+      CardService.newTextParagraph().setText("Calendar not found! Maybe it was deleted?"),
+    );
+  }
+
+  return section.addWidget(
+    CardService.newDecoratedText()
+      .setText(
+        `<b><font color="${googleCalendar.getColor()}">⬤</font> ${googleCalendar.getName()}</b>`,
+      )
+      .setBottomLabel(
+        "Use the linked calendar's settings (accessible in the left sidebar) to update sharing, notifications, and more.",
+      ),
+  );
 }
 
 export function CalendarCard(key: TeamCalendarKey, calendar: TeamCalendar) {
   return CardService.newCardBuilder()
     .setHeader(CalendarHeader(calendar))
     .addSection(CalendarSettingsSection(key, calendar))
+    .addSection(LinkedCalendarSection(calendar))
     .build();
 }
